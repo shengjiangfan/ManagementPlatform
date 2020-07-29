7abc7b3532b610acc04a11ecb3f17e407ba94867
yanyushan <yanyushan@outlook.com>
2020-07-29 11:26:24 +0800

    update

--- reactsimpleproject/src/App.js
+++ reactsimpleproject/src/App.js
@@ -9,7 +9,10 @@ class App extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            list:[],
+            list:[{
+              id:'',
+              name:''
+            }],
         }
     }
 
